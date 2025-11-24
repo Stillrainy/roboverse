4e0f187e00c385e718c06c443cf68065faacca77
Shuyuan Yang <sxy841@case.edu>
2025-11-23 22:53:16 -0500

    parse arg for large scale tuning

--- train/train_offline.py
+++ train/train_offline.py
@@ -2,30 +2,57 @@ import os
 import glob
 import numpy as np
 import gymnasium as gym
+import argparse
 from datetime import datetime
 
 import roboverse
 from roboverse.agents import OfflineAgentTrainer
 
-if __name__ == "__main__":
 
-    data_dir = 'data/data_Widow250DrawerRandomizedPickPlace-v0_100_noise_0.1_2025-11-23T17-17-43'
-    save_dir = os.path.join(
-        os.getcwd(),
-        'logs',
-        f'offline_{datetime.now().strftime("%Y%m%d_%H%M%S")}')
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train offline agent')
+    parser.add_argument('-d', '--data-dir', type=str, required=True,
+                        help='Path to input data directory')
+    parser.add_argument('--log-dir', type=str, default=None,
+                        help='Directory to write logs. If omitted, a timestamped folder under `logs/` in CWD will be used')
+    parser.add_argument('--obs-keys', nargs='+', default=['image', 'depth'],
+                        help='Observation keys to use (space-separated). Default: %(default)s')
+    parser.add_argument('--n-envs', type=int, default=10,
+                        help='Number of parallel environments')
+    parser.add_argument('-s', '--save-freq', type=int, default=1000,
+                        help='Model save frequency (in steps)')
+    parser.add_argument('-t', '--total-timesteps', type=int, default=100000,
+                        help='Total training timesteps')
+    parser.add_argument('--no-progress', action='store_true',
+                        help='Disable progress bar (enabled by default)')
+    parser.add_argument('-i', '--log-interval', type=int, default=None,
+                        help='Logging interval (None for default behavior)')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # compute default log dir if not provided
+    if args.log_dir is None:
+        save_dir = os.path.join(
+            os.getcwd(),
+            'logs',
+            f'offline_{datetime.now().strftime("%Y%m%d_%H%M%S")}')
+    else:
+        save_dir = args.log_dir
     os.makedirs(save_dir, exist_ok=True)
 
     trainer = OfflineAgentTrainer(
-        data_dir=data_dir,
-        obs_keys=['image', 'depth'],
+        data_dir=args.data_dir,
+        obs_keys=args.obs_keys,
         log_dir=save_dir,
-        n_envs=10,
-        save_freq=1_000,
+        n_envs=args.n_envs,
+        save_freq=args.save_freq,
     )
 
     trainer.train(
-        total_timesteps=100_000,
-        progress_bar=True,
-        log_interval=None,
+        total_timesteps=args.total_timesteps,
+        progress_bar=(not args.no_progress),
+        log_interval=args.log_interval,
     )
